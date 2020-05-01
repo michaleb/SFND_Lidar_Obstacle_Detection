44b156a30200383518f08fe86b584cf8d5b0ad0d
michaleb <michale.bailey@gmail.com>
2020-05-01 11:43:01 -0400

    Adjusted hyper params

--- src/environment.cpp
+++ src/environment.cpp
@@ -91,13 +91,13 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointCloud
   //renderPointCloud(viewer,inputCloud,"inputCloud");
 
   // Experiment with the ? values and find what works best
-  pcl::PointCloud<pcl::PointXYZI>::Ptr filterCloud = pointProcessorI->FilterCloud(inputCloud, 0.3, Eigen::Vector4f (-10, -5, -3, 1), Eigen::Vector4f ( 25, 6, 3, 1));//filtres 0.2, xmax 30
+  pcl::PointCloud<pcl::PointXYZI>::Ptr filterCloud = pointProcessorI->FilterCloud(inputCloud, 0.2, Eigen::Vector4f (-10, -5, -3, 1), Eigen::Vector4f ( 25, 6, 3, 1));//filtres 0.2, xmax 30
   std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> segmentCloud = pointProcessorI->SegmentPlane(filterCloud, 100, 0.35);//0.3 
   //renderPointCloud(viewer,filterCloud,"filterCloud");
 
   renderPointCloud(viewer,segmentCloud.second,"planeCloud",Color(0,1,0));
 
-    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(segmentCloud.first, 0.3, 5, 500);//0.3, 10, 500
+    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(segmentCloud.first, 0.3, 10, 500);//0.3, 10, 500
 
     int clusterId = 0;
     std::vector<Color> colors = {Color(1,0,0), Color(1,1,0), Color(0,0,1), Color(1,0,1), Color(1,1,1)};
